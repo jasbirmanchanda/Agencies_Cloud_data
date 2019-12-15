f2a66b4bb57ffdf5a9d919d305da9e8bc4dd27d4
jasbirmanchanda <46789676+jasbirmanchanda@users.noreply.github.com>
2019-12-15 18:38:53 -0500

    first commit

---
+++ learn.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongo = require('mongodb');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/api/students', (req,res)=> {
+	var MongoClient = mongo.MongoClient;
+	var url = "mongodb://localhost:27017/";
+
+var name = req.body.user;
+console.log(name+"------------------");
+	MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+	  if (err) throw err;
+    var dbo = db.db("local");
+    dbo.listCollections().toArray((err, items)=>{
+				console.log(items);
+	});
+	  dbo.collection("Student").find({}, { projection: { _id: 0, firstName: 1, lastName: 1 } }).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		res.send(result);
+		db.close();
+	  });
+	}); 
+});
+/*
+app.get('/api/students/:fname', (req,res)=> {
+	var MongoClient = require('mongodb').MongoClient;
+	var url = "mongodb://localhost:27017/";
+
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var dbo = db.db("mydb");
+	  dbo.collection("students").find({}, { projection: { _id: 0, fname: 1, lname: 1 } }).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		res.send(result);
+		db.close();
+	  });
+	}); 
+});
+
+*/
+app.get('/api/testo', (req,res)=> {
+res.send("Hi this is a test API for testo");
+});
+
+
+const port = process.env.PORT || 9099;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
---
+++ queries.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongo = require('mongodb');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.post('/api/agency', (req,res)=> {
+	var MongoClient = mongo.MongoClient;
+	var url = "mongodb+srv://jas:12345@cluster0-5zpp0.mongodb.net/test?retryWrites=true&w=majority";
+
+var id = req.body.id;
+var name = req.body.name;
+var gender = req.body.gender;
+var payRate = req.body.payrate;
+var address = req.body.adress;
+var contactNo = req.body.contact;
+var Nationality = req.body.nationality;
+
+	MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+	  if (err) throw err;
+	var dbo = db.db("Best_Employment_Agencies");
+
+	var col = {Employee_id : id, e_name : name, e_gender : gender, e_payRate : payRate, e_address : address, e_contactNo : contactNo, e_nationality : Nationality};
+
+	dbo.collection("StaffPlus").insertOne(col,function(err, respond){
+		if (err)
+			throw err;
+		
+		res.send("Document Inserted");
+		console.log('Connection Established');
+		db.close();
+	})
+	});
+
+});
+
+
+
+const port = process.env.PORT || 9099;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
