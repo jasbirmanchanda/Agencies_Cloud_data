0bba2f19ae9266b0510828e6db9ae2a3b94a0b45
jasbirmanchanda <46789676+jasbirmanchanda@users.noreply.github.com>
2019-12-15 19:05:43 -0500

    second commit

--- queries.js
+++ queries.js
@@ -21,7 +21,7 @@ var Nationality = req.body.nationality;
 
 	var col = {Employee_id : id, e_name : name, e_gender : gender, e_payRate : payRate, e_address : address, e_contactNo : contactNo, e_nationality : Nationality};
 
-	dbo.collection("StaffPlus").insertOne(col,function(err, respond){
+	dbo.collection("Adecco_Staffing").insertOne(col,function(err, respond){
 		if (err)
 			throw err;
 		
