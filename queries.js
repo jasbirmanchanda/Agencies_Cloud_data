935250ad9b7b5bfbdc2b7320d81da91348d92ac2
jasbirmanchanda <46789676+jasbirmanchanda@users.noreply.github.com>
2019-12-16 18:50:15 -0500

    UpdateName

--- queries.js
+++ queries.js
@@ -109,6 +109,37 @@ var agencyName = req.body.name;
 });
 
 
+//Update a perticulater data with perticular column
+app.post('/api/updateName', (req,res)=> {
+	var MongoClient = mongo.MongoClient;
+	var url = "mongodb+srv://jas:12345@cluster0-5zpp0.mongodb.net/test?retryWrites=true&w=majority";
+
+var id = req.body.id;
+var agencyName = req.body.name1;
+var name = req.body.name;
+
+
+	MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+	  if (err) throw err;
+	var dbo = db.db("Best_Employment_Agencies");
+	
+	var val = {Employee_id : id};
+	var newVal = { $set: {e_name: name, Employee_id: id}}
+
+	dbo.collection(agencyName).updateOne(val, newVal,function(err,result){
+		if (err)
+			throw err;
+		
+		res.send(result);
+		console.log("1 document updated");
+		db.close();
+	})
+	});
+
+});
+
+
+
 
 const port = process.env.PORT || 9099;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
